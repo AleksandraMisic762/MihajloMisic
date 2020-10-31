5e2ecbc525d3b4d319aecb26f845063f5fa84908
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2020-10-31 21:33:50 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -62,12 +62,12 @@ dugme.addEventListener('click', () =>{
                                             <td class="samoIzgradnja">${Math.round(popustProcenat[1] * 100)}%</td>
                                             <td class="oba">${Math.round(popustProcenat[2] * 100)}%</td>`);
     document.querySelector('#iznosPopusta').insertAdjacentHTML('beforeend' ,`
-                                            <td>Iznos popusta</td><td class="samoProjekat"> ${Math.round((parseInt(doprinosUradjeno) - parseInt(porezUkupno ))*popustProcenat[0])}</td>
-                                            <td class="samoIzgradnja"> ${Math.round(popustProcenat[1] * (parseInt(doprinosUradjeno) - parseInt(porezUkupno )))}</td>
+                                            <td>Iznos popusta</td><td class="samoProjekat"> ${Math.round((parseInt(doprinosUradjeno) - parseInt(y[0] ))*popustProcenat[0])}</td>
+                                            <td class="samoIzgradnja"> ${Math.round(popustProcenat[1] * (parseInt(doprinosUradjeno) - parseInt(y[1] )))}</td>
                                             <td class="oba"> ${Math.round(popustProcenat[2] * (parseInt(doprinosUradjeno) - parseInt(porezUkupno)))}</td>`);
     document.querySelector('#iznosNaknade').insertAdjacentHTML('beforeend' ,`
-                                            <td>Iznos naknade</td><td class="samoProjekat"> ${Math.round((parseInt(doprinosUradjeno) - parseInt(porezUkupno ))*(1-popustProcenat[0]))}</td>
-                                            <td class="samoIzgradnja"> ${Math.round((1-popustProcenat[1]) * (parseInt(doprinosUradjeno) - parseInt(porezUkupno )))}</td>
+                                            <td>Iznos naknade</td><td class="samoProjekat"> ${Math.round((parseInt(doprinosUradjeno) - parseInt(y[0] ))*(1-popustProcenat[0]))}</td>
+                                            <td class="samoIzgradnja"> ${Math.round((1-popustProcenat[1]) * (parseInt(doprinosUradjeno) - parseInt(y[1] )))}</td>
                                             <td class="oba"> ${Math.round((1-popustProcenat[2]) * (parseInt(doprinosUradjeno) - parseInt(porezUkupno)))}</td>`);                                        
                                         
 
